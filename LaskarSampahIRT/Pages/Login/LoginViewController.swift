96dd36dab9ceb81b50434ae2cce1090a4f29e21d
okky pribadi <donbosco_okky@yahoo.com>
2018-10-10 12:47:15 +0700

    Template Login

--- LaskarSampahIRT/Pages/Login/LoginViewController.swift
+++ LaskarSampahIRT/Pages/Login/LoginViewController.swift
@@ -8,23 +8,31 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController
+{
+    //MARK Outlet
+    @IBOutlet weak var logo: UIView!
+    @IBOutlet weak var txtEmailTlp: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    
+    //MARK Function
+    @IBAction func btnMasuk(_ sender: Any)
+    {
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnDaftar(_ sender: Any)
+    {
+        
     }
-    */
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
 
+    
+    
+    }
 }
