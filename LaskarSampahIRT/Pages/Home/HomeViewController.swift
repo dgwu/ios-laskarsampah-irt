a6ccd34cf59a6f08df648116268aaf56fbb85bc4
dgwu <danielgunawanwu@gmail.com>
2018-10-11 09:24:52 +0700

    home design

--- LaskarSampahIRT/Pages/Home/HomeViewController.swift
+++ LaskarSampahIRT/Pages/Home/HomeViewController.swift
@@ -8,23 +8,56 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
-
+class HomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet weak var newsAndTipsCollectionView: UICollectionView!
+    @IBOutlet weak var priceTableView: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        initialSetup()
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func initialSetup() {
+        newsAndTipsCollectionView.delegate = self
+        newsAndTipsCollectionView.dataSource = self
+        priceTableView.delegate = self
+        priceTableView.dataSource = self
     }
-    */
-
+    
+    // MARK: Collection View Delegate
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NewsAndTips", for: indexPath) as! NewsAndTipsCollectionViewCell
+        
+        cell.newsImageView.image = UIImage(named: "sample_img")
+        cell.newsLabel.text = "Test \(indexPath.row)"
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("select at \(indexPath.row)")
+    }
+    
+    // MARK: Table View Delegate
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dummyPriceList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PriceCell", for: indexPath)
+        let priceItem = dummyPriceList[indexPath.row]
+        cell.textLabel?.text = priceItem.description
+        cell.detailTextLabel?.text = "Rp \(priceItem.price)/\(priceItem.priceUnit)"
+        
+        return cell
+    }
 }
---
+++ LaskarSampahIRT/Pages/Home/NewsAndTipsCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NewsAndTipsCollectionViewCell.swift
+//  LaskarSampahIRT
+//
+//  Created by Daniel Gunawan on 10/10/18.
+//  Copyright © 2018 Daniel Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class NewsAndTipsCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var newsLabel: UILabel!
+    
+}
